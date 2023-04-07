138683183241d31b74a991acaa515b7f4eea2930
Invasion <invasion@deeplay.io>
2023-04-07 19:22:31 +0700

    Make second refactoring version

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { MatchResult } from './MatchResult';
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// Create an object that implements the DataReader interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an object of MatchReader and pass DataReader implementation
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (const match of reader.data) {
+for (const match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
