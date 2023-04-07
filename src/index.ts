1c536eb28805af4556de693ad21cbfdd543367bf
Invasion <invasion@deeplay.io>
2023-04-07 23:52:47 +0700

    Refactor analyzers and reporters data using composition

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import { MatchResult } from './MatchResult';
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
+import { ConsoleReport } from './ReportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
+import { HtmlReport } from './ReportTargets/HtmlReport';
 
 // Create an object that implements the DataReader interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -9,14 +12,5 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
-
-for (const match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} times`);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+summary.buildAndPrintReport(matchReader.matches);
