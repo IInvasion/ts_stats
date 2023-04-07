9acdf411edaf5da210b54410d919732dd25d3838
Invasion <invasion@deeplay.io>
2023-04-08 00:02:12 +0700

    Add static methods to MatchReader and Summary classes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './ReportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './ReportTargets/HtmlReport';
-
-// Create an object that implements the DataReader interface
-const csvFileReader = new CsvFileReader('football.csv');
 
 // Create an object of MatchReader and pass DataReader implementation
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 summary.buildAndPrintReport(matchReader.matches);
